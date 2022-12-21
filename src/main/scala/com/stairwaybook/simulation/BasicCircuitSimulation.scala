17f7a2a45fbc2afa411346beb745faa0a08dff64
Asher Oloo <ashermcoa@gmail.com>
2022-12-21 18:10:43 +0300

    Circuit simulation class

--- src/main/scala/com/stairwaybook/simulation/BasicCircuitSimulation.scala
+++ src/main/scala/com/stairwaybook/simulation/BasicCircuitSimulation.scala
@@ -24,39 +24,39 @@ abstract class BasicCircuitSimulation extends Simulation :
       actions = a :: actions
       a()
 
-    def inverter(input: Wire, ouput: Wire) =
-      def invertAction() =
-        val inputSig = input.getSignal
-        afterDelay(InverterDelay) {
-          ouput setSignal !inputSig
-        }
+  def inverter(input: Wire, ouput: Wire) =
+    def invertAction() =
+      val inputSig = input.getSignal
+      afterDelay(InverterDelay) {
+        ouput setSignal !inputSig
+      }
 
-      input addAction invertAction
+    input addAction invertAction
 
-    def andGate(a1: Wire, a2: Wire, output: Wire) =
-      def andAction() =
-        val a1Sig = a1.getSignal
-        val a2Sig = a2.getSignal
-        afterDelay(AndGateDelay) {
-          output setSignal (a1Sig & a2Sig)
-        }
+  def andGate(a1: Wire, a2: Wire, output: Wire) =
+    def andAction() =
+      val a1Sig = a1.getSignal
+      val a2Sig = a2.getSignal
+      afterDelay(AndGateDelay) {
+        output setSignal (a1Sig & a2Sig)
+      }
 
-      a1 addAction andAction
-      a2 addAction andAction
+    a1 addAction andAction
+    a2 addAction andAction
 
-    def orGate(o1: Wire, o2: Wire, output: Wire) =
-      def orAction() =
-        val o1Sig = o1.getSignal
-        val o2Sig = o2.getSignal
-        afterDelay(OrGateDelay) {
-          output setSignal (o1Sig & o2Sig)
-        }
+  def orGate(o1: Wire, o2: Wire, output: Wire) =
+    def orAction() =
+      val o1Sig = o1.getSignal
+      val o2Sig = o2.getSignal
+      afterDelay(OrGateDelay) {
+        output setSignal (o1Sig & o2Sig)
+      }
 
-      o1 addAction orAction
-      o2 addAction orAction
+    o1 addAction orAction
+    o2 addAction orAction
 
-    def probe(name: String, wire: Wire) =
-      def probAction() =
-        println(name + " " + currentTime +
-          " new-value = " + wire.getSignal)
-      wire addAction probAction
+  def probe(name: String, wire: Wire) =
+    def probAction() =
+      println(name + " " + currentTime +
+        " new-value = " + wire.getSignal)
+    wire addAction probAction

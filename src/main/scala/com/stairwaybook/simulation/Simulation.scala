7c1a0ebb905edf2cb6e046547e1b4e6c6143d8d3
omasher <ashermcoa@gmail.com>
2022-12-21 16:16:20 +0300

    Add simulation API

---
+++ src/main/scala/com/stairwaybook/simulation/Simulation.scala
@@ -0,0 +1,33 @@
+package com.stairwaybook.simulation
+
+abstract class Simulation:
+
+  type Action = () => Unit
+
+  case class WorkItem(time: Int, action: Action)
+
+  private var curtime = 0
+  def currentTime: Int = curtime
+
+  private var agenda: List[WorkItem] = List()
+
+  private def insert(ag: List[WorkItem], item: WorkItem): List[WorkItem] =
+    if ag.isEmpty || item.time < ag.head.time then item :: ag
+    else ag.head :: insert(ag.tail, item)
+
+  def afterDelay(delay: Int)(block: => Unit) =
+    val item = WorkItem(curtime + delay, () => block)
+    agenda = insert(agenda, item)
+
+  private def next() =
+    (agenda: @unchecked) match
+      case item :: rest =>
+        agenda = rest
+        curtime = item.time
+        item.action()
+
+  def run() =
+    afterDelay(0) {
+      println("*** simulation started, time = " + currentTime + " ***")
+    }
+    while !agenda.isEmpty do next()
